eb75db7848762356f4e3ffecfbd05315603cecc6
Erick Santos Umbelino de Farias <erick.f@academico.ifrn.edu.br>
2022-05-19 11:38:19 -0300

    Capítulo 01 alterações

---
+++ WebApplication1/WebApplication1/Models/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Categoria
+    {
+        public long CategoriaId { get; set; } // Identificar
+        public string Nome { get; set; }
+
+
+    }
+}
