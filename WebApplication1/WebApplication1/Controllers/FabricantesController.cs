07cb0d9502b7ad5a1d820a5c884afe9c12ea4019
Erick Santos Umbelino de Farias <erick.f@academico.ifrn.edu.br>
2022-05-30 10:29:09 -0300

    Novas alterações

---
+++ WebApplication1/WebApplication1/Controllers/FabricantesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class FabricantesController : Controller
+    {
+        private EFContext context = new EFContext();
+
+        // GET: Fabricantes
+        public ActionResult Index()
+        {
+            return View( context.Fabricantes.OrderBy(c => c.Nome)
+                );
+        }
+    }
+}
